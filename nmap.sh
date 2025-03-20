1ecd7f4c9be62459d4efa3eaad27f661d50e959f
rw9999 <134976895+rw9999@users.noreply.github.com>
2025-03-20 00:27:06 -0700

    Update nmap.sh

--- nmap.sh
+++ nmap.sh
@@ -1,22 +1,28 @@
 #! /bin/bash
 
+#checks if ip was enter in cli
 if [ -z "$1" ]; then
         echo "Please provide ip address"
         exit 1
 fi
 
+#puts it into ip variable
 ip="${1}"
 tcpPorts=""
 udpPorts=""
 
+#asks for file name
 read -p "Enter file name: " filename
 
+#creates file or overwrites if is exists
 > "$filename"
 
 echo "Scanning open TCP ports"
 
+# nmap scans all open ports/should be fast/verbose output while putting it in the file
 nmap -p- --min-rate=2000  $ip | tee -a "$filename"  
 
+#parses open ports
 while read line
 do
         if [[ ${line:0:1} =~ [0-9] ]]
@@ -28,6 +34,7 @@ done < "$filename"
 
 echo "Scanning protocols and versions on open TCP ports"
 
+#does udp ports
 nmap -p $tcpPorts -sC -sV $ip | tee -a "$filename"  
 
 echo "Scanning open UDP ports"
